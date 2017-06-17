84ecdda17fb67009ef36b5ea7a0cf9f2cb1d81fe
Diego Vargas <degva.29@gmail.com>
2017-06-16 20:36:07 -0500

    Added stuff

---
+++ lp2/PokemonAPI/PokemonAPI/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace PokemonAPI.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
